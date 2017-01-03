ef96ff8fef54880bceded05813ad27d4ece203e4
Lidiya suryaningsih <lidiyasuryaningsih020297@gmail.com>
2017-01-02 19:23:04 -0800

    TAMBAHAN MENU UTAMA

--- Apotek/Apotek/MainForm.vb
+++ Apotek/Apotek/MainForm.vb
@@ -3,4 +3,8 @@
     Private Sub PasienToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PasienToolStripMenuItem.Click
         Pasien.ShowDialog()
     End Sub
+
+    Private Sub ObatToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ObatToolStripMenuItem.Click
+        Resep.ShowDialog()
+    End Sub
 End Class
