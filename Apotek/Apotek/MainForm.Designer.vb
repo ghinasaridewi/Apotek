23138b02dc220263b4c500c7878e70ed2e762a83
GHINA-PC\GHINA <ghinasaridewilasanuddin@gmail.com>
2017-01-03 09:27:23 +0700

    MENU UTAMA

---
+++ Apotek/Apotek/MainForm.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MasterDataToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PasienToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ObatToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TransaksiToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ObatToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ApotekToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TentangToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MasterDataToolStripMenuItem, Me.ApotekToolStripMenuItem, Me.TentangToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(400, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MasterDataToolStripMenuItem
+        '
+        Me.MasterDataToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PasienToolStripMenuItem, Me.ObatToolStripMenuItem, Me.TransaksiToolStripMenuItem, Me.ObatToolStripMenuItem1})
+        Me.MasterDataToolStripMenuItem.Name = "MasterDataToolStripMenuItem"
+        Me.MasterDataToolStripMenuItem.Size = New System.Drawing.Size(82, 20)
+        Me.MasterDataToolStripMenuItem.Text = "Master Data"
+        '
+        'PasienToolStripMenuItem
+        '
+        Me.PasienToolStripMenuItem.Name = "PasienToolStripMenuItem"
+        Me.PasienToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.PasienToolStripMenuItem.Text = "Pasien"
+        '
+        'ObatToolStripMenuItem
+        '
+        Me.ObatToolStripMenuItem.Name = "ObatToolStripMenuItem"
+        Me.ObatToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ObatToolStripMenuItem.Text = "Resep"
+        '
+        'TransaksiToolStripMenuItem
+        '
+        Me.TransaksiToolStripMenuItem.Name = "TransaksiToolStripMenuItem"
+        Me.TransaksiToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.TransaksiToolStripMenuItem.Text = "Transaksi"
+        '
+        'ObatToolStripMenuItem1
+        '
+        Me.ObatToolStripMenuItem1.Name = "ObatToolStripMenuItem1"
+        Me.ObatToolStripMenuItem1.Size = New System.Drawing.Size(152, 22)
+        Me.ObatToolStripMenuItem1.Text = "Obat"
+        '
+        'ApotekToolStripMenuItem
+        '
+        Me.ApotekToolStripMenuItem.Name = "ApotekToolStripMenuItem"
+        Me.ApotekToolStripMenuItem.Size = New System.Drawing.Size(57, 20)
+        Me.ApotekToolStripMenuItem.Text = "Apotek"
+        '
+        'TentangToolStripMenuItem
+        '
+        Me.TentangToolStripMenuItem.Name = "TentangToolStripMenuItem"
+        Me.TentangToolStripMenuItem.Size = New System.Drawing.Size(63, 20)
+        Me.TentangToolStripMenuItem.Text = "Tentang"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(400, 367)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "MainForm"
+        Me.Text = "Menu Utama"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents MasterDataToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PasienToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ObatToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents TransaksiToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ObatToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ApotekToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents TentangToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
